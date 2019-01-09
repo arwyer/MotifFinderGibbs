ee8beabd4f5baef10c6b7cb8879276941792535c
arwyer <awyer@vols.utk.edu>
2019-01-09 10:49:59 -0500

    fixing dust parsing assumptions

--- lib/MotifFinderGibbs/Utils/FastaUtils.py
+++ lib/MotifFinderGibbs/Utils/FastaUtils.py
@@ -1,5 +1,19 @@
 import os
 
 def RemoveRepeats(path,newpath):
-    command = '/kb/deployment/bin/meme/bin/dust ' + path + ' > ' + newpath
+    tmp = '/kb/module/work/tmp/tmp.fa'
+    command = '/kb/deployment/bin/meme/bin/dust ' + path + ' > ' + tmp
     os.system(command)
+    newFasta = ''
+    with open(tmp,'r') as tmpFile:
+        newFasta = ''
+        sequence = ''
+        for line in tmpFile:
+            if '>' in line:
+                newFasta += sequence + '\n'
+                sequence = ''
+                newFasta += line.replace('\n','') + '\n'
+            else:
+                sequence += line.replace('\n','')
+    with open(newpath,'w') as newFile:
+        newFile.write(newFasta)
